c128bfa4691a72b54f82f507504fe73321940b2c
nKarad <n.karad@gmail.com>
2021-10-13 08:12:39 -0300

    primera subida

---
+++ main.js
@@ -0,0 +1,15 @@
+//Prompt
+var comidaFavorita = prompt("Cual es tu comida favorita?");
+console.log("Tu comida Favorita es: " + comidaFavorita);
+
+var bebidaFavorita = prompt("Cual es tu bebida favorita?");
+console.log("Tu bebida Favorita es: " + bebidaFavorita);
+
+
+//Alert
+alert("Tu comida favorita es : " + comidaFavorita);
+alert("Tu bebida favorita es : " + bebidaFavorita);
+
+const Y =" y "
+alert("Tu comida y bebida preferidas son " + comidaFavorita+Y+bebidaFavorita);
+console.log("Tu comida y bebida preferidas son " + comidaFavorita+Y+bebidaFavorita)
