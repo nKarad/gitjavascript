6321d36511bc1a28dd41e9f6dd6c852cb947a201
nKarad <n.karad@gmail.com>
2021-10-14 15:43:13 -0300

    subida 2

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 //Prompt
-var comidaFavorita = prompt("Cual es tu comida favorita?");
-console.log("Tu comida Favorita es: " + comidaFavorita);
+// var comidaFavorita = prompt("Cual es tu comida favorita?");
+// console.log("Tu comida Favorita es: " + comidaFavorita);
 
-var bebidaFavorita = prompt("Cual es tu bebida favorita?");
-console.log("Tu bebida Favorita es: " + bebidaFavorita);
+// var bebidaFavorita = prompt("Cual es tu bebida favorita?");
+// console.log("Tu bebida Favorita es: " + bebidaFavorita);
+
+// //Alert
+// alert("Tu comida favorita es : " + comidaFavorita);
+// alert("Tu bebida favorita es : " + bebidaFavorita);
+
+// const Y =" y "
+// alert("Tu comida y bebida preferidas son " + comidaFavorita+Y+bebidaFavorita);
+// console.log("Tu comida y bebida preferidas son " + comidaFavorita+Y+bebidaFavorita)
 
 
-//Alert
-alert("Tu comida favorita es : " + comidaFavorita);
-alert("Tu bebida favorita es : " + bebidaFavorita);
 
-const Y =" y "
-alert("Tu comida y bebida preferidas son " + comidaFavorita+Y+bebidaFavorita);
-console.log("Tu comida y bebida preferidas son " + comidaFavorita+Y+bebidaFavorita)
+//Clase 2
+let comidaFavorita = prompt("Cual es tu comida preferida?")
+if (comidaFavorita == "Pizza") {alert ("Sos un capo")
+}
+else if (comidaFavorita == "Pescado") {alert ("Me da impresion.")
+}
+ else {alert ("Tu comida preferida es " +comidaFavorita + ", no entendes nada")}
