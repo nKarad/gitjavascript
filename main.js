27eff04aca3d5b1d5f38741385817e32578a105e
nKarad <n.karad@gmail.com>
2021-10-19 13:14:09 -0300

    Tercer clase

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
-//Prompt
-// var comidaFavorita = prompt("Cual es tu comida favorita?");
-// console.log("Tu comida Favorita es: " + comidaFavorita);
+// Clase 2
+// let comidaFavorita = prompt("Cual es tu comida preferida?")
+// if (comidaFavorita == "Pizza") {alert ("Sos un capo")
+// }   
+// else if (comidaFavorita == "Pescado") {alert ("Me da impresion.")
+// }
+//  else {alert ("Tu comida preferida es " +comidaFavorita + ", no entendes nada")}
 
-// var bebidaFavorita = prompt("Cual es tu bebida favorita?");
-// console.log("Tu bebida Favorita es: " + bebidaFavorita);
+// let encuestaCliente =parseFloat(prompt("Cuanto pagarías por tu comida preferida"));
 
-// //Alert
-// alert("Tu comida favorita es : " + comidaFavorita);
-// alert("Tu bebida favorita es : " + bebidaFavorita);
-
-// const Y =" y "
-// alert("Tu comida y bebida preferidas son " + comidaFavorita+Y+bebidaFavorita);
-// console.log("Tu comida y bebida preferidas son " + comidaFavorita+Y+bebidaFavorita)
-
-
-
-//Clase 2
-let comidaFavorita = prompt("Cual es tu comida preferida?")
-if (comidaFavorita == "Pizza") {alert ("Sos un capo")
-}   
-else if (comidaFavorita == "Pescado") {alert ("Me da impresion.")
-}
- else {alert ("Tu comida preferida es " +comidaFavorita + ", no entendes nada")}
-
-let encuestaCliente =parseFloat(prompt("Cuanto pagarías por tu comida preferida"));
-
-if (encuestaCliente>1500) {alert("Estás loco");
-}
-else if (encuestaCliente>800) {alert("Buen precio");
-}
-else if (encuestaCliente>101) {alert("Un regalo");
-}
+// if (encuestaCliente>1500) {alert("Estás loco");
+// }
+// else if (encuestaCliente>800) {alert("Buen precio");
+// }
+// else if (encuestaCliente>101) {alert("Un regalo");
+// }
 // else {alert("Ratón")}
 
-let esMenor100 = (encuestaCliente<101)
-if (esMenor100) {alert("Ratón!")}
+// let esMenor100 = (encuestaCliente<101)
+// if (esMenor100) {alert("Ratón!")}
+ 
+
+//Clase 3
+let num = parseInt(prompt("Ingresa un numero"))
+// for(let i =1; i <=10;i++) {let resultado = num*i;
+// console.log(num+"X"+i*"="+resultado)
+// }
+
+for (let i=1; i<10; i++) {
+    if (i == 8 ) {continue;
+    }
+    {console.log (i);
+}   }
+
