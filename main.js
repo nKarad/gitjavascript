82da4ac30551965a0e7f940e592b0265ad44ccea
nKarad <n.karad@gmail.com>
2021-11-16 16:15:16 -0300

    ...

--- main.js
+++ main.js
@@ -401,78 +401,8 @@ let productContainer = document.querySelector(".products");
         <h4 class="basketTotal">$${cart},00</h4>
     </div>`
 
-deleteButtons();
-manageQuantity();
+
 }
 }
 
-//Flechitas//
 
-function manageQuantity() {
-let decreaseButtons = document.querySelectorAll('.decrease');
-let increaseButtons = document.querySelectorAll('.increase');
-let currentQuantity = 0;
-let currentProduct = '';
-let cartItems = localStorage.getItem('productsInCart');
-cartItems = JSON.parse(cartItems);
-
-for(let i=0; i < increaseButtons.length; i++) {
-decreaseButtons[i].addEventListener('click', () => {
-    console.log(cartItems);
-    currentQuantity = decreaseButtons[i].parentElement.querySelector('span').textContent;
-    console.log(currentQuantity);
-    currentProduct = decreaseButtons[i].parentElement.previousElementSibling.previousElementSibling.querySelector('span').textContent.toLocaleLowerCase().replace(/ /g,'').trim();
-    console.log(currentProduct);
-
-    if( cartItems[currentProduct].inCart > 1 ) {
-        cartItems[currentProduct].inCart -= 1;
-        cartNumbers(cartItems[currentProduct], "decrease");
-        totalCost(cartItems[currentProduct], "decrease");
-        localStorage.setItem('productsInCart', JSON.stringify(cartItems));
-        displayCart();
-    }
-});
-
-increaseButtons[i].addEventListener('click', () => {
-    console.log(cartItems);
-    currentQuantity = increaseButtons[i].parentElement.querySelector('span').textContent;
-    console.log(currentQuantity);
-    currentProduct = increaseButtons[i].parentElement.previousElementSibling.previousElementSibling.querySelector('span').textContent.toLocaleLowerCase().replace(/ /g,'').trim();
-    console.log(currentProduct);
-
-    cartItems[currentProduct].inCart += 1;
-    cartNumbers(cartItems[currentProduct]);
-    totalCost(cartItems[currentProduct]);
-    localStorage.setItem('productsInCart', JSON.stringify(cartItems));
-    displayCart();
-});
-}
-}
-
-function deleteButtons() {
-let deleteButtons = document.querySelectorAll('.product ion-icon');
-let productNumbers = localStorage.getItem('cartNumbers');
-let cartCost = localStorage.getItem("totalCost");
-let cartItems = localStorage.getItem('productsInCart');
-cartItems = JSON.parse(cartItems);
-let productName;
-console.log(cartItems);
-
-for(let i=0; i < deleteButtons.length; i++) {
-deleteButtons[i].addEventListener('click', () => {
-    productName = deleteButtons[i].parentElement.textContent.toLocaleLowerCase().replace(/ /g,'').trim();
-   
-    localStorage.setItem('cartNumbers', productNumbers - cartItems[productName].inCart);
-    localStorage.setItem('totalCost', cartCost - ( cartItems[productName].price * cartItems[productName].inCart));
-
-    delete cartItems[productName];
-    localStorage.setItem('productsInCart', JSON.stringify(cartItems));
-
-    displayCart();
-    onLoadCartNumbers();
-})
-}
-}
-
-onLoadCartNumbers();
-displayCart();
