bbcf21a15351c97a175fe74bf24e4722c58228af
nKarad <n.karad@gmail.com>
2021-11-21 23:39:15 -0300

    ...

--- main.js
+++ main.js
@@ -1,227 +1,3 @@
-// Clase 2
-// let comidaFavorita = prompt("Cual es tu comida preferida?")
-// if (comidaFavorita == "Pizza") {alert ("Sos un capo")
-// }
-// else if (comidaFavorita == "Pescado") {alert ("Me da impresion.")
-// }
-//  else {alert ("Tu comida preferida es " +comidaFavorita + ", no entendes nada")}
-
-// let encuestaCliente =parseFloat(prompt("Cuanto pagarías por tu comida preferida"));
-
-// if (encuestaCliente>1500) {alert("Estás loco");
-// }
-// else if (encuestaCliente>800) {alert("Buen precio");
-// }
-// else if (encuestaCliente>101) {alert("Un regalo");
-// }
-// else {alert("Ratón")}
-
-// let esMenor100 = (encuestaCliente<101)
-// if (esMenor100) {alert("Ratón!")}
-
-
-//Clase 3
-// let num = parseInt(prompt("Ingresa un numero"))
-// for(let i =1; i <=10;i++) {let resultado = num*i;
-// console.log(num+"X"+i*"="+resultado)
-// }
-
-// for (let i=1; i<10; i++) {
-//     if (i == 8 ) {continue;
-//     }
-//     {console.log (i);
-// }   }
-
-// for (let i=1 ; i <=10; i++) {
-//     let resultado = num*i;
-//     console.log (num + "x" + i + "=" + resultado);
-// }
-
-// for (let i=1; i<=10; i++) {
-//     if (i % 2 ==0){console.log(i + "es par")  ;
-//     }
-//     else {console.log (i + "es impar")}
-// }
-
-// let entrada = prompt ("Querés salir del bucle?");
-// while (entrada!="si") {alert ("El usuario no quiere salir")}
-// entrada= prompt ("Seguro?");
-
-
-//NO SALE
-
-// let pass = 1234;
-// let dato = parseInt(prompt("ingrese su pass"));
-// let intentos = 3;
-
-// while ((dato != pass) && (intentos > 0)) {
-//     alert("Contraseña in co rrec ta");
-//     dato =paserlInt(prompt("Nono, de nuevo"));
-//     intentos--;}
-
-
-
-
-
-// Clase 4
-
-// function resta (num1, num2) {
-//     console.log (num1-num2)
-//     let resultado= num1-num2;
-//     return resultado
-// }
-
-// resta (8,7)
-
-
-
-// //Calcular IVA
-
-// let precio= parseInt(prompt("Ingresa el valor del producto"))
-
-
-// //Iva 21%
-// function precioConsumidor(precio, impuesto ){
-// let precioConsumidor = precio + (precio*impuesto/100);
-// console.log (precioConsumidor);
-// return precioConsumidor
-// }
-// ReadableStream
-// precioConsumidor (precio, 21)
-
-// //Iva alimentos 8%
-// function precioConsumidor(precio, impuesto ){
-//     let precioConsumidor = precio + (precio*impuesto/100);
-//     console.log (precioConsumidor);
-//     return precioConsumidor
-//     }
-
-// precioConsumidor (precio, 8)
-
-
-// const libro = {
-//     titulo: "La Metamorfosis",
-//     genero: "Realismo mágico",
-//     autor: {Nombre: "Fanz",
-//             Apellido: "Kafka"},
-//     añoPublicacion: "1912"
-//     }
-// console.log(libro);
-// console.log(libro.autor)
-
-
-
-//ENTREGA CLASE 5//
-
-// function Biblioteca (titulo, genero,autor, añoPublicacion) {
-//     this.titulo = titulo;
-//     this.genero = genero;
-//     this.autor = autor;
-//     this.año = añoPublicacion;
-//     this.fichaTecnica = () => {
-//         console.log ("El último libro en tu colección es " +this.titulo + " y pertenece al género " + this.genero + ", fue escrito por " + this.autor +" en el año " +this.año  )
-//     }
-// }
-
-// // const metamorfosis = new Biblioteca ("La Metamofrosis", "Realismo mágico", "Franz Kafka", "1912")
-// // console.log (metamorfosis)
-
-
-// const agregarLibro =() =>{
-
-// let titulo = prompt("Cuál es el titulo del nuevo libro que compraste?")
-// let genero = prompt("A que genero pertenece el libro")
-// let autor = prompt ("Quien lo escribió")
-// let añoPublicacion   =prompt("En que año se publicó?")
-
-// const libro2 = new Biblioteca (titulo, genero, autor, añoPublicacion);
-// console.log(libro2)
-// console.log(libro2.fichaTecnica())
-// }
-
-
-// let librosParaNiños = ["Harry Potter", "El señor de los anillos", "Juego de tronos", "Crepusculo"]
-// console.log (librosParaNiños)
-// librosParaNiños.push ("Narnia")
-// console.log (librosParaNiños)
-// // librosParaNiños.sort
-// // // let agregarLibro = prompt("Que libro le gusta a tus hijos?")
-// // // librosParaNiños.push (agregarLibro)
-
-// // let librosParaAdultos = ["De Sexualidades publicas y Universidades privadas", "El Aleph", "50 sombras de gray"]
-// // let librosParaAdolescentes = ["Catcher in the rye", "Player N5"]
-
-// // let catalogo = librosParaNiños.concat(librosParaAdultos, librosParaAdolescentes)
-// // console.log (catalogo)
-
-
-// class libroNuevo {
-//     constructor (titulo, genero, autor, año) {
-//             this.titulo = titulo;
-//             this.genero = genero;
-//             this.autor = autor;
-//             this.año = año;
-// }
-// }
-
-// let biblioteca = []
-
-// const agregarLibro = () =>  {
-// let tituloNuevo=prompt("Cuál es el titulo del nuevo libro que compraste?");
-// let generoNuevo=prompt("A que genero pertenece el libro");
-// let autorNuevo=prompt ("Quien lo escribió");
-// let añoNuevo=prompt("En que año se publicó?");
-
-// let libro = new libroNuevo (tituloNuevo, generoNuevo, autorNuevo, añoNuevo)
-// biblioteca.push(libro)}
-// console.log(biblioteca)
-
-
-//CLASE 8
-
-// let titulo= document.createElement ("h2");
-// titulo.setAttribute ("class", "miH2");
-// titulo.innerHTML= "<a href=https://www.google.com>Link a Google</a>"
-// document.getElementById("prueba").appendChild(titulo)
-// console.log(titulo)
-
-// //CLASE 9
-// const parrafo = document.querySelector("#parrafo");
-// const boton = document.querySelector("#btn");
-
-// const escribir =() =>{
-// let texto = prompt ("Como es tu nombre?")
-// parrafo.textContent ="Gracias por tu mensaje " + texto + "!!"}
-
-// boton.addEventListener("click", escribir)
-
-// nombre.onchange = () =>
-// {console.log ("Cambio el nombre a "+nombre.value )}
-// // texto.onkeydown = () =>
-// // // {console.log (texto.value)}
-// const escribir = () => {
-//     let Nombre = prompt("escribi");
-//     parrafo.textContent = texto
-// }
-// // boton.addEventListener("click", escribir);
-
-// const selector = document.querySelector ("#selector");
-// selector.addEventListener ("change", ()=>{
-//     console.log (selector.value)
-// })
-
-// const enlace= document.querySelector("#enlace")
-// enlace.onlcick = (e) => {
-//     e.preventDefault();
-// }
-
-// //Movimientos
-
-// boton.addEventListener("mousemove", () => {console.log("Hay movimiento");
-// });
-
-
-
 //Formulario de contacto
 
 // let form = document.getElementsByTagName("form")[0];
@@ -233,7 +9,7 @@
 
 let carts = document.querySelectorAll(".add-cart")
 
-let products = [{
+const products = [{
     name: "Jeresy Bulls",
     tag: "bulls",
     price: 2000,
@@ -241,7 +17,7 @@ let products = [{
 },
 
 {
-    name: "Bulls Alt.",
+    name: "Bulls Altern.",
     tag: "bullsAlt",
     price: 1000,
     incCart: 0
@@ -369,7 +145,7 @@ let cartItems = localStorage.getItem("productsInCart");
 cartItems=JSON.parse(cartItems);
 // console.log(cartItems)
 let productContainer = document.querySelector(".products");
-
+let cartCost =localStorage.getItem("totalCost");
 
     // console.log ("A vergaston")
 
@@ -379,26 +155,39 @@ let productContainer = document.querySelector(".products");
     productContainer.innerHTML ="";
     Object.values(cartItems).map(item =>{
         productContainer.innerHTML+= `
+        
         <div class="product" >
         <ion-icon name="close-circle"></ion-icon>
         <img src="./assets/${item.tag}.png">
         <span>${item.name}</span>
         </div>  
-        <div class=price>$${item.price}</div>
 
+
+        <div class=price>$${item.price}</div>
 
     
         <div class=quantity>
-         <ion-icon name="arrow-dropleft-circle"></ion-icon>
+        <ion-icon name="remove-circle"></ion-icon>
         ${item.incCart}
-        <ion-icon name="arrow-dropright-circle"></ion-icon>
+        <ion-icon name="add-circle"></ion-icon>
         </div>
 
 
         <div class=total >$${item.incCart * item.price }.00</div>
         `
-    })
- 
+        
+    });
+
+    productContainer.innerHTML += 
+    `<div class=containerSumaTotal>
+     <h4 class= textoSuma>
+     Total Productos    
+     </h4>
+     
+     <h4 sumaProductos> 
+     $${cartCost},00 </h4>        
+     </div>
+     `
 
 
 }
