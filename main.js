c21f9746b31975cf45adf8c3cb4a1c7792dd88d4
nKarad <n.karad@gmail.com>
2021-11-23 06:42:02 -0300

    ...

--- main.js
+++ main.js
@@ -7,12 +7,15 @@
 
 //Carrito
 
-let carrito = document.querySelectorAll(".add-cart")
+let carrito = document.querySelectorAll(".agregarCamiseta")
 
 //Por que no toma el prevent default?
 // $("#camiseta").on ("añadirCarrito", function(e){
 // e.preventdefault();
 // })
+
+
+//Camisetas
 
 const productos = [{
     nombre: "Jeresy Bulls",
@@ -22,7 +25,7 @@ const productos = [{
 },
 
 {
-    nombre: "Bulls Altern.",
+    nombre: "Bulls Alternat.",
     tag: "bullsAlt",
     precio: 1000,
     enCarrito: 0
@@ -69,97 +72,103 @@ const productos = [{
     enCarrito: 0
 },
 ]
+
+
+//
+
 for (let i = 0; i < carrito.length; i++) {
     carrito[i].addEventListener("click", () => {
-        cartNumbers(productos[i]);
+        numeroIcono(productos[i]);
         costoTotal(productos[i])
     })
 }
-function onLoadCartNumbers() {
-    let productNumbers = localStorage.getItem("cartNumbers")
+function onLoadnumeroIcono() {
+    let productNumbers = localStorage.getItem("numeroIcono")
     if (productNumbers) { document.querySelector(".cart span").textContent = productNumbers }
 }
 
-function cartNumbers(producto) {
+function numeroIcono(producto) {
     // console.log(" a vergaston ", producto)
-    let productNumbers = localStorage.getItem("cartNumbers");
+    let productNumbers = localStorage.getItem("numeroIcono");
 
     productNumbers = parseInt(productNumbers);
 
     if (productNumbers) {
-        localStorage.setItem("cartNumbers", productNumbers + 1);
+        localStorage.setItem("numeroIcono", productNumbers + 1);
         document.querySelector(".cart span").textContent = productNumbers + 1;
     }
     else {
-        localStorage.setItem("cartNumbers", 1);
+        localStorage.setItem("numeroIcono", 1);
         document.querySelector(".cart span").textContent = 1;
     }
 
     setItem(producto);
 }
 
-   function setItem(producto){
-let cartItems = localStorage.getItem("productsInCart")
-cartItems =JSON.parse(cartItems)
-// console.log("mis items son ", cartItems)
+function setItem(producto) {
+    let cartItems = localStorage.getItem("productsInCart")
+    cartItems = JSON.parse(cartItems)
+    // console.log("mis items son ", cartItems)
 
 
-if (cartItems!=null){
+    if (cartItems != null) {
 
-     if( cartItems[producto.tag] == undefined) {
-         cartItems= {
-             ...cartItems,
-             [producto.tag]:producto
+        if (cartItems[producto.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [producto.tag]: producto
             }
         }
 
 
-    cartItems[producto.tag].enCarrito+=1;
-}
-else{
-    producto.enCarrito = 1;
-    cartItems={
-    [producto.tag]:producto
-}
-}
+        cartItems[producto.tag].enCarrito += 1;
+    }
+    else {
+        producto.enCarrito = 1;
+        cartItems = {
+            [producto.tag]: producto
+        }
+    }
 
-localStorage.setItem("productsInCart", JSON.stringify (cartItems))
-}
-
-function costoTotal(producto){
-
-    let costoTotal =localStorage.getItem("costoTotal")
-// console.log ("Costo total es ", costoTotal)
-;
-
-console.log("el costo total es", costoTotal);
-console.log(typeof costoTotal);
-
-if (costoTotal != null){
-    costoTotal = parseInt(costoTotal);
-    localStorage.setItem("costoTotal", costoTotal+ producto.precio);
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems))
 }
 
-else {localStorage.setItem("costoTotal", producto.precio )}
+
+
+function costoTotal(producto) {
+
+    let costoTotal = localStorage.getItem("costoTotal")
+        // console.log ("Costo total es ", costoTotal)
+        ;
+
+    console.log("el costo total es", costoTotal);
+    console.log(typeof costoTotal);
+
+    if (costoTotal != null) {
+        costoTotal = parseInt(costoTotal);
+        localStorage.setItem("costoTotal", costoTotal + producto.precio);
+    }
+
+    else { localStorage.setItem("costoTotal", producto.precio) }
 
 
 }
 
-function displayCart(){
-let cartItems = localStorage.getItem("productsInCart");
-cartItems=JSON.parse(cartItems);
-// console.log(cartItems)
-let containerProductos = document.querySelector(".productos");
-let costoTotal =localStorage.getItem("costoTotal");
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+
+    // console.log(cartItems)
+
+    let containerProductos = document.querySelector(".productos");
+    let costoTotal = localStorage.getItem("costoTotal");
 
     // console.log ("A vergaston")
 
-    //Agregar o quitar items desde el carrito linea 391
-    //Tarjetas de items 
     if (cartItems && containerProductos) {
-        containerProductos.innerHTML ="";
-    Object.values(cartItems).map(item =>{
-        containerProductos.innerHTML+= `
+        containerProductos.innerHTML = "";
+        Object.values(cartItems).map(item => {
+            containerProductos.innerHTML += `
         
         <div class="producto" >
         <ion-icon name="close-circle"></ion-icon>
@@ -178,13 +187,15 @@ let costoTotal =localStorage.getItem("costoTotal");
         </div>
 
 
-        <div class=total >$${item.enCarrito * item.precio }.00</div>
+        <div class=total >$${item.enCarrito * item.precio}.00</div>
         `
-        
-    });
 
-    containerProductos.innerHTML += 
-    `<div class=containerSumaTotal>
+        });
+
+        //Total de todo el carrito
+
+        containerProductos.innerHTML +=
+            `<div class=containerSumaTotal>
      <h4 class= textoSuma>
      Total Productos    
      </h4>
@@ -195,9 +206,10 @@ let costoTotal =localStorage.getItem("costoTotal");
      `
 
 
-}
+    }
 }
 
 
-onLoadCartNumbers();
+
+onLoadnumeroIcono();
 displayCart();
